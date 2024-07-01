5ec370186f3f1836f6e9036baef4d4d7e89f281d
JimmyMP <brayan.cuyate.m@uni.pe>
2024-06-30 23:53:40 -0500

    ADD

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def predict():
     return render_template('index.html', data=data_table ,graph1='uploads/observado.png', graph2='uploads/predicciones.png')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT", default=5000))
